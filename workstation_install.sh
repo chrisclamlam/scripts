070d8ed09722fc6fb3bd42ce648c2ec2b2770a06
Chris Lam <lam.chris.clam@gmail.com>
2020-11-06 11:20:29 -0800

    Create workstation_install.sh

---
+++ workstation_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install visual-studio-code
+brew cask install spotify
+brew cask install anydesk
